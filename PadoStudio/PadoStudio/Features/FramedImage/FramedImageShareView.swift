2ae1bc3626c9711787f5685ace1e4c9d84d61e61
Yujin Jeon <166476876+romiwaves@users.noreply.github.com>
2025-05-30 23:56:57 +0900

    프레임 사진 보여주기 화면 구현, 홈 버튼 연결, 공유 구현

---
+++ PadoStudio/PadoStudio/Features/FramedImage/FramedImageShareView.swift
@@ -0,0 +1,18 @@
+//
+//  FramedImageShareView.swift
+//  PadoStudio
+//
+//  Created by gabi on 5/30/25.
+//
+
+import SwiftUI
+
+struct FramedImageShareView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FramedImageShareView()
+}
