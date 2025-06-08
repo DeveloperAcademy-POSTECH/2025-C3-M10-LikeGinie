2dc044da7dd6b44e5c1613cd154ed91750db5f49
Min kyeong <y.minkyeong@gmail.com>
2025-06-08 20:24:00 +0900

    0608 MainTextView 기능 개선

--- PadoStudio/PadoStudio/Features/Home/MainTextView.swift
+++ PadoStudio/PadoStudio/Features/Home/MainTextView.swift
@@ -9,21 +9,31 @@ import SwiftUI
 
 struct MainTextView: View {
     let proxy: GeometryProxy
+    @Environment(\.horizontalSizeClass) var sizeClass
+
+    var body: some View {
+        if sizeClass == .regular {
+            HomePadLayout(proxy: proxy)
+        } else {
+            HomePhoneLayout(proxy: proxy)
+        }
+    }
+}
+
+// 아이폰용 레이아웃
+struct HomePhoneLayout: View {
+    let proxy: GeometryProxy
 
     var body: some View {
         HStack(alignment: .center, spacing: 12) {
             Image("ic_home_title")
                 .resizable()
                 .scaledToFit()
-                .frame(
-                    width: proxy.size.width * 0.15
-                )
+                .frame(width: proxy.size.width * 0.15)
 
             VStack(alignment: .center) {
                 Text("파도 사진관")
-                    .font(
-                        .largeSinchonTitleResponsive(size: 30, proxy: proxy)
-                    )
+                    .font(.largeSinchonTitleResponsive(size: 30, proxy: proxy))
                     .foregroundColor(.white)
                     .multilineTextAlignment(.center)
 
@@ -37,7 +47,36 @@ struct MainTextView: View {
         .frame(maxWidth: .infinity)
         .padding(.top, 40)
         .padding(.horizontal)
+    }
+}
 
+// 아이패드용 레이아웃
+struct HomePadLayout: View {
+    let proxy: GeometryProxy
+
+    var body: some View {
+        HStack(alignment: .center, spacing: 24) {
+            Image("ic_home_title")
+                .resizable()
+                .scaledToFit()
+                .frame(width: proxy.size.width * 0.10) // 패드에서 더 작게
+
+            VStack(alignment: .center) {
+                Text("파도 사진관")
+                    .font(.largeSinchonTitleResponsive(size: 30, proxy: proxy)) // 더 크게
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+
+                Text("직접 만든 서핑 캐릭터와 함께 사진을 찍어보세요!")
+                    .font(.title3RegularResponsive(size: 10, proxy: proxy)) // 더 크게
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+            }
+            .frame(alignment: .center)
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.top, 80) // 패드에서 더 넓게
+        .padding(.horizontal, 80)
     }
 }
 
