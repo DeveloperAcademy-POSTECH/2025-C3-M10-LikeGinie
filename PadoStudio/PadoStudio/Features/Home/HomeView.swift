121116d7edda1fdf02a699fabddb65e9b686a0d6
Min kyeong <y.minkyeong@gmail.com>
2025-06-09 11:47:39 +0900

    0609 원띵, 커비, 체셔, 세라 선생님 감사합니다

--- PadoStudio/PadoStudio/Features/Home/HomeView.swift
+++ PadoStudio/PadoStudio/Features/Home/HomeView.swift
@@ -3,10 +3,10 @@ import SwiftUI
 struct HomeView: View {
     @EnvironmentObject var navModel: NavigationViewModel
     @Environment(\.horizontalSizeClass) var sizeClass
-    
+
     var body: some View {
         if sizeClass == .regular {
-            HomePadLayout() // 파라미터 제거
+            HomePadLayout()  // 파라미터 제거
                 .environmentObject(navModel)
         } else {
             HomePhoneLayout()
@@ -17,41 +17,47 @@ struct HomeView: View {
 
 struct HomePadLayout: View {
     @EnvironmentObject var navModel: NavigationViewModel
-    
+
     var body: some View {
         GeometryReader { proxy in
-            let topPadding = proxy.size.height * 0.002 // 더 작은 값으로 조정
+            let topPadding = proxy.size.height * 0.002  // 더 작은 값으로 조정
             let carouselHeight = proxy.size.height * 0.28
             let carouselBottomPadding = proxy.size.height * 0.04
             let buttonBottomPadding: CGFloat = 40.0
-            
+
             ZStack {
                 Image("bg_home")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                     .ignoresSafeArea()
-                
+
                 VStack(spacing: 0) {
                     // 상단 텍스트
-                    MainTextView(proxy: proxy) // 내부 GeometryReader의 proxy 사용
+                    MainTextView(proxy: proxy)  // 내부 GeometryReader의 proxy 사용
                         .padding(.top, proxy.size.height * 0.08)
                         .padding(.horizontal, 100)
-                    
+
                     Spacer().frame(height: proxy.size.height * 0.18)
-                    
+
                     // 캐러셀
                     InfiniteCarouselView(images: ImageDataService.fetchImages())
                         .frame(height: 300)
-                    
+
                     Spacer()
-                    
+
                     MainButtonView(
-                        onCameraTapped: { navModel.navigate(to: .startRecording) },
+                        onCameraTapped: {
+                            navModel.navigate(to: .startRecording)
+                        },
                         onGalleryTapped: { navModel.navigate(to: .gallery) }
                     )
                     .padding(.bottom, buttonBottomPadding)
                 }
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                .frame(
+                    maxWidth: .infinity,
+                    maxHeight: .infinity,
+                    alignment: .top
+                )
             }
         }
     }
@@ -59,7 +65,7 @@ struct HomePadLayout: View {
 
 struct HomePhoneLayout: View {
     @EnvironmentObject var navModel: NavigationViewModel
-    
+
     var body: some View {
         GeometryReader { proxy in
             let topPadding = proxy.size.height * 0.08
@@ -67,67 +73,37 @@ struct HomePhoneLayout: View {
             let buttonBottomPadding: CGFloat = 30.0
             //            let isWide = proxy.size.width > 400
             //            let spacing = isWide ? proxy.size.width * 0.06 : proxy.size.width * 0.03
-            
+            let getCenter = (proxy.size.width * 0.5)
             ZStack {
                 // 배경 이미지
                 Image("bg_home")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                     .ignoresSafeArea()
-                
+
                 // 본문 콘텐츠
                 VStack(spacing: 0) {
                     MainTextView(proxy: proxy)
                         .padding(.top, topPadding)
                         .padding(.horizontal, 50)
-                    
+
                     Spacer().frame(height: proxy.size.height * 0.18)
-                    
+
                     InfiniteCarouselView(images: ImageDataService.fetchImages())
                         .frame(height: 150)
-                    
+
                     Spacer()
-                    
-                    HStack {
-                        Spacer()
-                        CameraButton(
-                            action: { navModel.navigate(to: .startRecording) },
-                            size: 100
-                        )
-                       
-                        
-                        GalleryButton(
-                            action: { navModel.navigate(to: .gallery) },
-                            size: 80
-                        )
-                        Spacer(minLength: proxy.size.width * 0.05)
-                    }
-//                    
-//                    HStack {
-//                        Spacer()
-//                        GalleryButton(
-//                            action: { navModel.navigate(to: .gallery) },
-//                            size: 80
-//                        )
-//                        Spacer(minLength: proxy.size.width * 0.05)
-//                    }
-                    
-                }
-                
-                // 버튼 레이어
-//                VStack {
-                  
-//                    ZStack {
-                        // 카메라 버튼 - 중앙
-                      
-                        
-                        // 갤러리 버튼 - 오른쪽
-                       
-                  //  }
-                    //                    .padding(.horizontal, 24)
-//                    .padding(.bottom, buttonBottomPadding)
-                //}
-            }
+
+                    MainButtonView(
+                        onCameraTapped: {
+                            navModel.navigate(to: .startRecording)
+                        },
+                        onGalleryTapped: { navModel.navigate(to: .gallery) }
+                    )
+                    .padding(.bottom, buttonBottomPadding)
+
+                }.frame(width: proxy.size.width)
+            }.frame(width: proxy.size.width)
         }
     }
 }
